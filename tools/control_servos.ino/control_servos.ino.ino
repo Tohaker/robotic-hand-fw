e2c74956fcb31327fb7e6b9d1beefd8770051ca3
milesbardon <milesbardon@gmail.com>
2019-07-31 20:46:57 +0100

    Tidied up code. Ready for hacking!

--- tools/control_servos.ino/control_servos.ino.ino
+++ tools/control_servos.ino/control_servos.ino.ino
@@ -1,3 +1,4 @@
+
 /*************************************************** 
   This is an example for our Adafruit 16-channel PWM & Servo driver
   Servo test - this will drive 8 servos, one after the other on the
@@ -27,33 +28,30 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  143 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVONEUTRAL  307
-#define SERVOMAX  491 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN 150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOHALFMIN 229
+#define SERVONEUTRAL 307
+#define SERVOHALFMAX 394
+#define SERVOMAX 480 // this is the 'maximum' pulse length count (out of 4096)
 
 #define THUMB 0
 #define INDEX 2
 #define MIDDLE 4
 #define LITTLE 6
 
-// our servo # counter
-uint8_t servonum = 6;
-
 void setup() {
-  pwm.begin();
-  
+  pwm.begin();  
   pwm.setPWMFreq(50);  // Digital servos run at 50 Hz updates
 
   delay(10);
-  
-  homeAllServos`();
-  
-  delay(1000);
-  
-  pullBackAllServos();
-  
+
+  neutral();
+
+  // Clench the fist, then unclench.
+  homeAllServos();
   delay(2000);
-  
+  pullBackAllServos();
+  delay(2000);
   homeAllServos();
 }
 
@@ -74,7 +72,7 @@ void setServoPulse(uint8_t n, double pulse) {
 }
 
 void loop() {
-  
+
 }
 
 void neutral() {
@@ -84,16 +82,46 @@ void neutral() {
   pwm.setPWM(LITTLE, 0, SERVONEUTRAL);
 }
 
-void homeAllServos() {
+void allMaxHalf() {
+  pwm.setPWM(THUMB, 0, SERVOHALFMAX);
+  pwm.setPWM(INDEX, 0, SERVOHALFMAX);
+  pwm.setPWM(MIDDLE, 0, SERVOHALFMAX);
+  pwm.setPWM(LITTLE, 0, SERVOHALFMAX);
+}
+
+void allMinHalf() {
+  pwm.setPWM(THUMB, 0, SERVOHALFMIN);
+  pwm.setPWM(INDEX, 0, SERVOHALFMIN);
+  pwm.setPWM(MIDDLE, 0, SERVOHALFMIN);
+  pwm.setPWM(LITTLE, 0, SERVOHALFMIN);
+}
+
+void allMax() {
   pwm.setPWM(THUMB, 0, SERVOMAX);
-  pwm.setPWM(INDEX, 0, SERVOMIN);
-  pwm.setPWM(MIDDLE, 0, SERVOMIN);
+  pwm.setPWM(INDEX, 0, SERVOMAX);
+  pwm.setPWM(MIDDLE, 0, SERVOMAX);
   pwm.setPWM(LITTLE, 0, SERVOMAX);
 }
 
+void allMin() {
+  pwm.setPWM(THUMB, 0, SERVOMIN);
+  pwm.setPWM(INDEX, 0, SERVOMIN);
+  pwm.setPWM(MIDDLE, 0, SERVOMIN);
+  pwm.setPWM(LITTLE, 0, SERVOMIN);
+}
+
+// These configurations are based entirely on the physical setup of your arm. 
+// Change the order if need be.
 void pullBackAllServos() {
   pwm.setPWM(THUMB, 0, SERVOMIN);
-  pwm.setPWM(INDEX, 0, SERVOMAX);
+  pwm.setPWM(INDEX, 0, SERVOMIN);
   pwm.setPWM(MIDDLE, 0, SERVOMAX);
+  pwm.setPWM(LITTLE, 0, SERVOMAX);
+}
+
+void homeAllServos() {
+  pwm.setPWM(THUMB, 0, SERVOMAX);
+  pwm.setPWM(INDEX, 0, SERVOMAX);
+  pwm.setPWM(MIDDLE, 0, SERVOMIN);
   pwm.setPWM(LITTLE, 0, SERVOMIN);
 }
